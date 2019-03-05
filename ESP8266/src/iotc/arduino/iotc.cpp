b082f34a84cf0cc935cd0c2205d1ed9e2d64e556
PProvost <peter@provost.org>
2019-03-05 12:46:28 -0700

    Fixed SSL/TLS lookup to use setTrustAnchors

--- ESP8266/src/iotc/arduino/iotc.cpp
+++ ESP8266/src/iotc/arduino/iotc.cpp
@@ -20,8 +20,10 @@ unsigned long _getNow()
 retry_getNow:
     IPAddress address(129, 6, 15, retryCount % 2 == 1 ? 28 : 29); // time.nist.gov NTP server
     WiFiUDP Udp;
-    if (Udp.begin(2390) == 0) {
-        if (retryCount < 5) {
+    if (Udp.begin(2390) == 0)
+    {
+        if (retryCount < 5)
+        {
             retryCount++;
             goto retry_getNow;
         }
@@ -30,14 +32,14 @@ retry_getNow:
     }
 
     memset(packetBuffer, 0, NTP_PACKET_SIZE);
-    packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-    packetBuffer[1] = 0;     // Stratum, or type of clock
-    packetBuffer[2] = 6;     // Polling Interval
-    packetBuffer[3] = 0xEC;  // Peer Clock Precision
-    packetBuffer[12]  = 49;
-    packetBuffer[13]  = 0x4E;
-    packetBuffer[14]  = 49;
-    packetBuffer[15]  = 52;
+    packetBuffer[0] = 0b11100011; // LI, Version, Mode
+    packetBuffer[1] = 0;          // Stratum, or type of clock
+    packetBuffer[2] = 6;          // Polling Interval
+    packetBuffer[3] = 0xEC;       // Peer Clock Precision
+    packetBuffer[12] = 49;
+    packetBuffer[13] = 0x4E;
+    packetBuffer[14] = 49;
+    packetBuffer[15] = 52;
     Udp.beginPacket(address, 123);
     Udp.write(packetBuffer, NTP_PACKET_SIZE);
     Udp.endPacket();
@@ -45,15 +47,19 @@ retry_getNow:
     // wait to see if a reply is available
     WAITMS(1000);
 
-    if (Udp.parsePacket() ) {
+    if (Udp.parsePacket())
+    {
         Udp.read(packetBuffer, NTP_PACKET_SIZE);
         unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
         unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
         unsigned long secsSince1900 = highWord << 16 | lowWord;
 
         retVal = (secsSince1900 - 2208988800UL);
-    } else {
-        if (retryCount < 5) {
+    }
+    else
+    {
+        if (retryCount < 5)
+        {
             retryCount++;
             goto retry_getNow;
         }
@@ -65,31 +71,36 @@ retry_getNow:
 }
 
 static unsigned long g_udpTime = 0, g_lastRead = 0;
-unsigned long getNow() {
+unsigned long getNow()
+{
     unsigned long ms = millis();
-    if (g_udpTime == 0 || ms - g_lastRead > NTP_SYNC_PERIOD) {
+    if (g_udpTime == 0 || ms - g_lastRead > NTP_SYNC_PERIOD)
+    {
         g_udpTime = _getNow();
         g_lastRead = ms;
     }
     return g_udpTime + ((ms - g_lastRead) / 1000);
 }
 
-int _getOperationId(const char* dpsEndpoint, const char* scopeId, const char* deviceId,
-    const char* authHeader, char *operationId, char *hostName) {
+int _getOperationId(const char *dpsEndpoint, const char *scopeId, const char *deviceId,
+                    const char *authHeader, char *operationId, char *hostName)
+{
     ARDUINO_WIFI_SSL_CLIENT client;
     int exitCode = 0;
 
 #ifndef USES_WIFI101
-#ifndef AXTLS_DEPRECATED
-    client.setCACert((const uint8_t*)SSL_CA_PEM_DEF, strlen((const char*)SSL_CA_PEM_DEF));
-#else // AXTLS_DEPRECATED
-    #error "This sample requires esp8266 API version 2.4.2. You can select this version from Arduino IDE / Board manager"
-#endif // AXTLS_DEPRECATED
-#endif // USES_WIFI101
-
+    client.setCACert((const uint8_t *)SSL_CA_PEM_DEF, strlen((const char *)SSL_CA_PEM_DEF));
+#endif
+#ifdef ESP8266
+    BearSSL::X509List certList(SSL_CA_PEM_DEF);
+    client.setX509Time(g_udpTime);
+    client.setTrustAnchors(&certList);
+#endif
     int retry = 0;
-    while (retry < 5 && !client.connect(dpsEndpoint, AZURE_HTTPS_SERVER_PORT)) retry++;
-    if (!client.connected()) {
+    while (retry < 5 && !client.connect(dpsEndpoint, AZURE_HTTPS_SERVER_PORT))
+        retry++;
+    if (!client.connected())
+    {
         IOTC_LOG(F("ERROR: DPS endpoint %s call has failed."), hostName == NULL ? "PUT" : "GET");
         return 1;
     }
@@ -99,7 +110,8 @@ int _getOperationId(const char* dpsEndpoint, const char* scopeId, const char* de
     deviceIdEncoded.urlEncode();
 
     size_t size = 0;
-    if (hostName == NULL) {
+    if (hostName == NULL)
+    {
         size = strlen("{\"registrationId\":\"%s\"}") + strlen(deviceId) - 2;
         size = snprintf(*tmpBuffer, STRING_BUFFER_1024, "\
 PUT /%s/registrations/%s/register?api-version=2018-11-01 HTTP/1.1\r\n\
@@ -113,9 +125,11 @@ connection: close\r\n\
 \r\n\
 {\"registrationId\":\"%s\"}\r\n\
     ",
-        scopeId, *deviceIdEncoded, dpsEndpoint,
-        AZURE_IOT_CENTRAL_CLIENT_SIGNATURE, size, authHeader, deviceId);
-    } else {
+                        scopeId, *deviceIdEncoded, dpsEndpoint,
+                        AZURE_IOT_CENTRAL_CLIENT_SIGNATURE, size, authHeader, deviceId);
+    }
+    else
+    {
         size = snprintf(*tmpBuffer, STRING_BUFFER_1024, "\
 GET /%s/registrations/%s/operations/%s?api-version=2018-11-01 HTTP/1.1\r\n\
 Host: %s\r\n\
@@ -125,17 +139,19 @@ accept: */*\r\n\
 %s\r\n\
 connection: close\r\n\
 \r\n",
-        scopeId, *deviceIdEncoded, operationId, dpsEndpoint,
-        AZURE_IOT_CENTRAL_CLIENT_SIGNATURE, authHeader);
+                        scopeId, *deviceIdEncoded, operationId, dpsEndpoint,
+                        AZURE_IOT_CENTRAL_CLIENT_SIGNATURE, authHeader);
     }
     assert(size != 0 && size < STRING_BUFFER_1024);
     tmpBuffer.setLength(size);
 
     client.println(*tmpBuffer);
     int index = 0;
-    while(!client.available()) {
+    while (!client.available())
+    {
         WAITMS(100);
-        if (index++ > IOTC_SERVER_RESPONSE_TIMEOUT * 10) {
+        if (index++ > IOTC_SERVER_RESPONSE_TIMEOUT * 10)
+        {
             // 20 secs..
             client.stop();
             IOTC_LOG(F("ERROR: DPS (%s) request has failed. (Server didn't answer within 20 secs.)"), hostName == NULL ? "PUT" : "GET");
@@ -145,29 +161,36 @@ connection: close\r\n\
 
     index = 0;
     bool enableSaving = false;
-    while (client.available() && index < STRING_BUFFER_1024 - 1) {
-        char ch = (char) client.read();
-        if (ch == '{') {
+    while (client.available() && index < STRING_BUFFER_1024 - 1)
+    {
+        char ch = (char)client.read();
+        if (ch == '{')
+        {
             enableSaving = true; // don't use memory for headers
         }
 
-        if (enableSaving) {
+        if (enableSaving)
+        {
             (*tmpBuffer)[index++] = ch;
         }
     }
     tmpBuffer.setLength(index);
 
-    const char* lookFor = hostName == NULL ? "{\"operationId\":\"" : "\"assignedHub\":\"";
+    const char *lookFor = hostName == NULL ? "{\"operationId\":\"" : "\"assignedHub\":\"";
     index = tmpBuffer.indexOf(lookFor, strlen(lookFor), 0);
-    if (index == -1) {
-error_exit:
+    if (index == -1)
+    {
+    error_exit:
         IOTC_LOG(F("ERROR: DPS (%s) request has failed.\r\n%s"), hostName == NULL ? "PUT" : "GET", *tmpBuffer);
         exitCode = 1;
         goto exit_operationId;
-    } else {
+    }
+    else
+    {
         index += strlen(lookFor);
         int index2 = tmpBuffer.indexOf("\"", 1, index + 1);
-        if (index2 == -1) goto error_exit;
+        if (index2 == -1)
+            goto error_exit;
         tmpBuffer.setLength(index2);
         strcpy(hostName == NULL ? operationId : hostName, (*tmpBuffer) + index);
     }
@@ -177,24 +200,29 @@ exit_operationId:
     return exitCode;
 }
 
-int getHubHostName(const char* dpsEndpoint, const char *scopeId, const char* deviceId, const char* key, char *hostName) {
+int getHubHostName(const char *dpsEndpoint, const char *scopeId, const char *deviceId, const char *key, char *hostName)
+{
     AzureIOT::StringBuffer authHeader(STRING_BUFFER_256);
     size_t size = 0;
 
     IOTC_LOG(F("- iotc.dps : getting auth..."));
-    if (getDPSAuthString(scopeId, deviceId, key, *authHeader, STRING_BUFFER_256, size)) {
+    if (getDPSAuthString(scopeId, deviceId, key, *authHeader, STRING_BUFFER_256, size))
+    {
         IOTC_LOG(F("ERROR: getDPSAuthString has failed"));
         return 1;
     }
     IOTC_LOG(F("- iotc.dps : getting operation id..."));
     AzureIOT::StringBuffer operationId(STRING_BUFFER_64);
     int retval = 0;
-    if ((retval = _getOperationId(dpsEndpoint, scopeId, deviceId, *authHeader, *operationId, NULL)) == 0) {
+    if ((retval = _getOperationId(dpsEndpoint, scopeId, deviceId, *authHeader, *operationId, NULL)) == 0)
+    {
         WAITMS(4000);
         IOTC_LOG(F("- iotc.dps : getting host name..."));
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; i++)
+        {
             retval = _getOperationId(dpsEndpoint, scopeId, deviceId, *authHeader, *operationId, hostName);
-            if (retval == 0) break;
+            if (retval == 0)
+                break;
             WAITMS(3000);
         }
     }
@@ -202,16 +230,19 @@ int getHubHostName(const char* dpsEndpoint, const char *scopeId, const char* dev
     return retval;
 }
 
-static void messageArrived(char* topic, byte* data, unsigned int length) {
-    handlePayload((char*)data, length, topic, topic ? strlen(topic) : 0);
+static void messageArrived(char *topic, byte *data, unsigned int length)
+{
+    handlePayload((char *)data, length, topic, topic ? strlen(topic) : 0);
 }
 
 /* extern */
-int iotc_free_context(IOTContext ctx) {
+int iotc_free_context(IOTContext ctx)
+{
     MUST_CALL_AFTER_INIT(ctx);
 
-    IOTContextInternal *internal = (IOTContextInternal*)ctx;
-    if (internal->endpoint != NULL) {
+    IOTContextInternal *internal = (IOTContextInternal *)ctx;
+    if (internal->endpoint != NULL)
+    {
         free(internal->endpoint);
     }
 
@@ -223,8 +254,9 @@ int iotc_free_context(IOTContext ctx) {
     return 0;
 }
 
-int iotc_connect(IOTContext ctx, const char* scope, const char* keyORcert,
-  const char* deviceId, IOTConnectType type) {
+int iotc_connect(IOTContext ctx, const char *scope, const char *keyORcert,
+                 const char *deviceId, IOTConnectType type)
+{
 
     CHECK_NOT_NULL(ctx)
     GET_LENGTH_NOT_NULL(keyORcert, 512);
@@ -232,59 +264,70 @@ int iotc_connect(IOTContext ctx, const char* scope, const char* keyORcert,
     AzureIOT::StringBuffer hostName;
     AzureIOT::StringBuffer username;
     AzureIOT::StringBuffer password;
-    IOTContextInternal *internal = (IOTContextInternal*)ctx;
+    IOTContextInternal *internal = (IOTContextInternal *)ctx;
 
-    if (type == IOTC_CONNECT_CONNECTION_STRING) {
+    if (type == IOTC_CONNECT_CONNECTION_STRING)
+    {
         getUsernameAndPasswordFromConnectionString(keyORcert,
-            strlen(keyORcert), hostName, internal->deviceId, username, password);
-    } else if (type == IOTC_CONNECT_SYMM_KEY) {
+                                                   strlen(keyORcert), hostName, internal->deviceId, username, password);
+    }
+    else if (type == IOTC_CONNECT_SYMM_KEY)
+    {
         assert(scope != NULL && deviceId != NULL);
         AzureIOT::StringBuffer tmpHostname(STRING_BUFFER_128);
-        if (getHubHostName(internal->endpoint == NULL ?
-                DEFAULT_ENDPOINT : internal->endpoint, scope, deviceId, keyORcert, *tmpHostname)) {
+        if (getHubHostName(internal->endpoint == NULL ? DEFAULT_ENDPOINT : internal->endpoint, scope, deviceId, keyORcert, *tmpHostname))
+        {
             return 1;
         }
         AzureIOT::StringBuffer cstr(STRING_BUFFER_256);
         int rc = snprintf(*cstr, STRING_BUFFER_256,
-            "HostName=%s;DeviceId=%s;SharedAccessKey=%s", *tmpHostname, deviceId, keyORcert);
+                          "HostName=%s;DeviceId=%s;SharedAccessKey=%s", *tmpHostname, deviceId, keyORcert);
         assert(rc > 0 && rc < STRING_BUFFER_256);
         cstr.setLength(rc);
 
         // TODO: move into iotc_dps and do not re-parse from connection string
         getUsernameAndPasswordFromConnectionString(*cstr, rc, hostName, internal->deviceId, username, password);
-    } else if (type == IOTC_CONNECT_X509_CERT) {
+    }
+    else if (type == IOTC_CONNECT_X509_CERT)
+    {
         IOTC_LOG(F("ERROR: IOTC_CONNECT_X509_CERT NOT IMPLEMENTED"));
-        connectionStatusCallback(IOTC_CONNECTION_DEVICE_DISABLED, (IOTContextInternal*)ctx);
+        connectionStatusCallback(IOTC_CONNECTION_DEVICE_DISABLED, (IOTContextInternal *)ctx);
         return 1;
     }
 
     internal->tlsClient = new ARDUINO_WIFI_SSL_CLIENT();
 
 #ifndef USES_WIFI101
-#ifndef AXTLS_DEPRECATED
-    internal->tlsClient->setCACert((const uint8_t*)SSL_CA_PEM_DEF, strlen((const char*)SSL_CA_PEM_DEF));
-#else // AXTLS_DEPRECATED
-    #error "This sample requires esp8266 API version 2.4.2. You can select this version from Arduino IDE / Board manager"
-#endif // AXTLS_DEPRECATED
-#endif // USES_WIFI101
+    internal->tlsClient->setCACert((const uint8_t *)SSL_CA_PEM_DEF, strlen((const char *)SSL_CA_PEM_DEF));
+#endif
+#ifdef ESP8266
+    BearSSL::X509List certList(SSL_CA_PEM_DEF);
+    internal->tlsClient->setX509Time(g_udpTime);
+    internal->tlsClient->setTrustAnchors(&certList);
+#endif
 
     internal->mqttClient = new PubSubClient(*hostName, AZURE_MQTT_SERVER_PORT, internal->tlsClient);
     internal->mqttClient->setCallback(messageArrived);
 
     int retry = 0;
-    while(retry < 10 && !internal->mqttClient->connected()) {
-        if (internal->mqttClient->connect(*internal->deviceId, *username, *password)) {
+    while (retry < 10 && !internal->mqttClient->connected())
+    {
+        if (internal->mqttClient->connect(*internal->deviceId, *username, *password))
+        {
             break;
-        } else {
+        }
+        else
+        {
             WAITMS(2000);
             retry++;
         }
     }
 
-    if (!internal->mqttClient->connected()) {
+    if (!internal->mqttClient->connected())
+    {
         IOTC_LOG(F("ERROR: MQTT client connect attempt failed. Check host, deviceId, username and password. (state %d)"),
-            internal->mqttClient->state());
-        connectionStatusCallback(IOTC_CONNECTION_BAD_CREDENTIAL, (IOTContextInternal*)ctx);
+                 internal->mqttClient->state());
+        connectionStatusCallback(IOTC_CONNECTION_BAD_CREDENTIAL, (IOTContextInternal *)ctx);
         delete internal->tlsClient;
         delete internal->mqttClient;
         internal->tlsClient = NULL;
@@ -296,28 +339,30 @@ int iotc_connect(IOTContext ctx, const char* scope, const char* keyORcert,
     buffer.setLength(snprintf(*buffer, 63, "devices/%s/messages/events/#", *internal->deviceId));
 
     int errorCode = 0;
-    if ( (errorCode = internal->mqttClient->subscribe(*buffer)) == 0)
+    if ((errorCode = internal->mqttClient->subscribe(*buffer)) == 0)
         IOTC_LOG(F("ERROR: mqttClient couldn't subscribe to %s. error code => %d"), *buffer, errorCode);
 
     buffer.setLength(snprintf(*buffer, 63, "devices/%s/messages/devicebound/#", *internal->deviceId));
 
-    if ( (errorCode = internal->mqttClient->subscribe(*buffer)) == 0)
+    if ((errorCode = internal->mqttClient->subscribe(*buffer)) == 0)
         IOTC_LOG(F("ERROR: mqttClient couldn't subscribe to %s. error code => %d"), *buffer, errorCode);
 
-    errorCode  = internal->mqttClient->subscribe("$iothub/twin/PATCH/properties/desired/#"); // twin desired property changes
-    errorCode += internal->mqttClient->subscribe("$iothub/twin/res/#"); // twin properties response
+    errorCode = internal->mqttClient->subscribe("$iothub/twin/PATCH/properties/desired/#"); // twin desired property changes
+    errorCode += internal->mqttClient->subscribe("$iothub/twin/res/#");                     // twin properties response
     errorCode += internal->mqttClient->subscribe("$iothub/methods/POST/#");
 
-    if (errorCode < 3) {
+    if (errorCode < 3)
+    {
         IOTC_LOG(F("ERROR: mqttClient couldn't subscribe to twin/methods etc. error code sum => %d"), errorCode);
     }
 
-    connectionStatusCallback(IOTC_CONNECTION_OK, (IOTContextInternal*)ctx);
+    connectionStatusCallback(IOTC_CONNECTION_OK, (IOTContextInternal *)ctx);
 
     iotc_do_work(internal);
-    const char* twin_topic = "$iothub/twin/GET/?$rid=0";
+    const char *twin_topic = "$iothub/twin/GET/?$rid=0";
     internal->messageId++; // next rid=1
-    if (mqtt_publish(internal, twin_topic, strlen(twin_topic), " ", 1) != 0) {
+    if (mqtt_publish(internal, twin_topic, strlen(twin_topic), " ", 1) != 0)
+    {
         IOTC_LOG(F("ERROR: Couldn't send the TWIN update request message"));
     }
     iotc_do_work(internal);
@@ -325,34 +370,40 @@ int iotc_connect(IOTContext ctx, const char* scope, const char* keyORcert,
 }
 
 /* extern */
-int iotc_disconnect(IOTContext ctx) {
+int iotc_disconnect(IOTContext ctx)
+{
     CHECK_NOT_NULL(ctx)
 
-    IOTContextInternal *internal = (IOTContextInternal*)ctx;
+    IOTContextInternal *internal = (IOTContextInternal *)ctx;
     MUST_CALL_AFTER_CONNECT(internal);
 
-    if (internal->mqttClient) {
-        if(internal->mqttClient->connected()) {
+    if (internal->mqttClient)
+    {
+        if (internal->mqttClient->connected())
+        {
             internal->mqttClient->disconnect();
         }
         delete internal->mqttClient;
         internal->mqttClient = NULL;
     }
 
-    connectionStatusCallback(IOTC_CONNECTION_DISCONNECTED, (IOTContextInternal*)ctx);
+    connectionStatusCallback(IOTC_CONNECTION_DISCONNECTED, (IOTContextInternal *)ctx);
     return 0;
 }
 
 /* extern */
-int iotc_do_work(IOTContext ctx) {
+int iotc_do_work(IOTContext ctx)
+{
     CHECK_NOT_NULL(ctx)
 
-    IOTContextInternal *internal = (IOTContextInternal*)ctx;
+    IOTContextInternal *internal = (IOTContextInternal *)ctx;
     MUST_CALL_AFTER_CONNECT(internal);
 
-    if (!internal->mqttClient->loop()) {
-        if (!internal->mqttClient->connected()) {
-            connectionStatusCallback(IOTC_CONNECTION_DISCONNECTED, (IOTContextInternal*)ctx);
+    if (!internal->mqttClient->loop())
+    {
+        if (!internal->mqttClient->connected())
+        {
+            connectionStatusCallback(IOTC_CONNECTION_DISCONNECTED, (IOTContextInternal *)ctx);
             delete internal->mqttClient;
             internal->mqttClient = NULL;
         }
@@ -362,7 +413,8 @@ int iotc_do_work(IOTContext ctx) {
 }
 
 /* extern */
-int iotc_set_network_interface(void* networkInterface) {
+int iotc_set_network_interface(void *networkInterface)
+{
     // NO-OP
     return 0;
 }
