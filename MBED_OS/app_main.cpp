6cad39d264540f63e9b03ef78e9ab477285413be
ludruda <ludruda@microsoft.com>
2019-04-15 16:08:01 +0200

    change main

--- MBED_OS/app_main.cpp
+++ MBED_OS/app_main.cpp
@@ -14,10 +14,9 @@
 #define WIFI_SSID ""
 #define WIFI_PASSWORD ""
 
-const char *scopeId = "";
-const char *deviceId = "";
-const char *deviceKey = "";
-
+const char *scopeId = "0ne00028D69";
+const char *deviceId = "ublox";
+const char *deviceKey = "Z0gDJBpRekNOiyPK4MOMrTwPyD7k/gjoKO2mOodXG9k=";
 
 static IOTContext context = NULL;
 static bool isConnected = false;
@@ -51,18 +50,18 @@ void loop()
         unsigned long ms = us_ticker_read() / 1000;
         if (ms - prevMillis > 15000)
         { // send telemetry every 15 seconds
-            char msg[64] = {0};
+            char msg[128] = {0};
             int pos = 0, errorCode = 0;
 
             prevMillis = ms;
             if (loopId++ % 2 == 0)
             { // send telemetry
-                pos = snprintf(msg, sizeof(msg) - 1, "{\"accelerometerX\": %d}", 10 + (rand() % 20));
+                pos = snprintf(msg, sizeof(msg) - 1, "{\"accelerometerX\": %d,\"accelerometerY\": %d,\"accelerometerZ\": %d,\"temp\": %d}", 10 + (rand() % 20), 6 + (rand() % 20), 18 + (rand() % 20), 10 + (rand() % 20));
                 errorCode = iotc_send_telemetry(context, msg, pos);
             }
             else
             { // send property
-                pos = snprintf(msg, sizeof(msg) - 1, "{\"dieNumber\":%d}", 1 + (rand() % 5));
+                pos = snprintf(msg, sizeof(msg) - 1, "{\"sold\":%d}", 1 + (rand() % 5));
                 errorCode = iotc_send_property(context, msg, pos);
             }
             msg[pos] = 0;
@@ -93,90 +92,87 @@ int main(int argc, char *argv[])
 
     LOG_VERBOSE("Network interface opened successfully.");
     WiFiInterface *wifi = network->wifiInterface();
+    EthInterface *eth = network->ethInterface();
+    CellularBase *cell = network->cellularBase();
+    printf("Available interfaces...");
     if (wifi)
     {
-        printf("This is a Wi-Fi board.");
-        // call WiFi-specific methods
+        printf("WIFI,");
     }
-    else
+    if (eth)
     {
-        EthInterface *eth = network->ethInterface();
-        if (eth)
-        {
-            LOG_VERBOSE("This is an ETH board.");
-            network->connect();
-            LOG_VERBOSE("Ip address: %s", network->get_ip_address());
+        printf("ETHERNET,");
+    }
+    if (cell)
+    {
+        printf("CELLULAR");
+    }
+    network->connect();
+    LOG_VERBOSE("Ip address: %s", network->get_ip_address());
 
-            // call WiFi-specific methods
-        }
-        CellularBase *cell = network->cellularBase();
-        if (cell)
+    // call WiFi-specific methods
+
+AzureIOT::StringBuffer routerAddress(network->get_ip_address(), strlen(network->get_ip_address()));
+{
+    int dotIndex = 0, foundCount = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        int pre = dotIndex;
+        dotIndex = routerAddress.indexOf(".", 1, dotIndex + 1);
+        if (dotIndex > pre)
         {
-            printf("This is a Cell board.");
-            // call WiFi-specific methods
+            foundCount++;
         }
     }
-    AzureIOT::StringBuffer routerAddress(network->get_ip_address(), strlen(network->get_ip_address()));
+
+    if (foundCount != 3 || dotIndex + 1 > routerAddress.getLength())
     {
-        int dotIndex = 0, foundCount = 0;
-        for (int i = 0; i < 3; i++)
-        {
-            int pre = dotIndex;
-            dotIndex = routerAddress.indexOf(".", 1, dotIndex + 1);
-            if (dotIndex > pre)
-            {
-                foundCount++;
-            }
-        }
-
-        if (foundCount != 3 || dotIndex + 1 > routerAddress.getLength())
-        {
-            LOG_ERROR("IPv4 address is expected."
-                      "(retval: %s foundCount:%d dotIndex:%d)",
-                      *routerAddress, foundCount, dotIndex);
-            return 1;
-        }
-
-        routerAddress.set(dotIndex + 1, '1');
-        if (routerAddress.getLength() > dotIndex + 1)
-        {
-            routerAddress.setLength(dotIndex + 2);
-        }
-        LOG_VERBOSE("routerAddress is assumed at %s", *routerAddress);
-    }
-    SocketAddress socketAddress(*routerAddress);
-    network->add_dns_server(socketAddress);
-    iotc_set_logging(IOTC_LOGGING_ALL);
-    iotc_set_network_interface(network);
-
-    int errorCode = iotc_init_context(&context);
-    if (errorCode != 0)
-    {
-        LOG_ERROR("Error initializing IOTC. Code %d", errorCode);
+        LOG_ERROR("IPv4 address is expected."
+                  "(retval: %s foundCount:%d dotIndex:%d)",
+                  *routerAddress, foundCount, dotIndex);
         return 1;
     }
 
-    //iotc_set_logging(IOTC_LOGGING_API_ONLY);
-
-    // for the simplicity of this sample, used same callback for all the events below
-    iotc_on(context, "MessageSent", onEvent, NULL);
-    iotc_on(context, "Command", onEvent, NULL);
-    iotc_on(context, "ConnectionStatus", onEvent, NULL);
-    iotc_on(context, "SettingsUpdated", onEvent, NULL);
-    iotc_on(context, "Error", onEvent, NULL);
-    LOG_VERBOSE("Connecting to IoTCentral");
-    //errorCode = iotc_connect(context, scopeId, connstring, deviceId, IOTC_CONNECT_CONNECTION_STRING);
-    errorCode = iotc_connect(context, scopeId, deviceKey, deviceId, IOTC_CONNECT_SYMM_KEY);
-    if (errorCode != 0)
+    routerAddress.set(dotIndex + 1, '1');
+    if (routerAddress.getLength() > dotIndex + 1)
     {
-        LOG_ERROR("Error @ iotc_connect. Code %d", errorCode);
-        return 1;
+        routerAddress.setLength(dotIndex + 2);
     }
-    LOG_VERBOSE("----------------Client is connected --------------------------");
-    prevMillis = us_ticker_read() / 1000;
+    LOG_VERBOSE("routerAddress is assumed at %s", *routerAddress);
+}
+SocketAddress socketAddress(*routerAddress);
+network->add_dns_server(socketAddress);
+iotc_set_logging(IOTC_LOGGING_ALL);
+iotc_set_network_interface(network);
 
-    while (true)
-        loop();
-
+int errorCode = iotc_init_context(&context);
+if (errorCode != 0)
+{
+    LOG_ERROR("Error initializing IOTC. Code %d", errorCode);
     return 1;
+}
+
+//iotc_set_logging(IOTC_LOGGING_API_ONLY);
+
+// for the simplicity of this sample, used same callback for all the events below
+iotc_on(context, "MessageSent", onEvent, NULL);
+iotc_on(context, "Command", onEvent, NULL);
+iotc_on(context, "ConnectionStatus", onEvent, NULL);
+iotc_on(context, "SettingsUpdated", onEvent, NULL);
+iotc_on(context, "Error", onEvent, NULL);
+LOG_VERBOSE("Connecting to IoTCentral");
+//errorCode = iotc_connect(context, scopeId, connstring, deviceId, IOTC_CONNECT_CONNECTION_STRING);
+errorCode = iotc_connect(context, scopeId, deviceKey, deviceId, IOTC_CONNECT_SYMM_KEY);
+if (errorCode != 0)
+{
+    LOG_ERROR("Error @ iotc_connect. Code %d", errorCode);
+    return 1;
+}
+LOG_VERBOSE("----------------Client is connected --------------------------");
+prevMillis = us_ticker_read() / 1000;
+
+while (true)
+    loop();
+
+return 1;
 }
